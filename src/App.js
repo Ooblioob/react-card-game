c9997519b52853ecf36711fa3fb3468d987e1c5e
Daniel Davis <ooblioob@gmail.com>
2020-04-03 09:45:46 -0400

    Project setup; basic card flipping

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './Card.js'
+import _ from 'lodash';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { 
+      cards: this.randomizeCards(9)
+    }
+    this.handleCardClick = this.handleCardClick.bind(this);
+  }
+  
+  randomizeCards = (n) => {
+    const suits = ['C', 'S', 'H', 'D'];
+    const numbers = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']; 
+    return [...Array(9)].map((e, i) => { 
+      return { 
+        id: i,
+        value: numbers[_.random(numbers.length-1)]+suits[_.random(suits.length-1)],
+        flipped: false
+      }
+    });
+  }
+
+  handleCardClick(index) {
+    const cards = this.state.cards.slice();
+    cards[index].flipped = !cards[index].flipped
+    this.setState({cards: cards});
+  }
+  
+  render () {
+      return (
+      <div>
+        <h1>Play the Game!</h1>
+        <button onClick={() => console.log("button click")}>Start Over?</button>
+        <button>Shuffle</button>
+        <button>Unflip</button>
+        <div className="container">
+          { this.state.cards.map((card, i) => <Card key={card.id} index={i} value={card.value} flipped={card.flipped} onClicked={this.handleCardClick} />) } 
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+import './Card.css';
+
+export default function Card(props) {
+  // const [flipped, setFlipped] = useState(false);
+  const flipCard = e => {
+    props.onClicked(props.index); // call the parent's click handler
+  }
+
+  const imageSrc = "img/cards/"+props.value+".jpg";
+
+  return (
+    <div onClick={flipCard} className={["card", props.flipped?"isFlipped":""].join(" ")}>
+      <div className="front">
+        <img src="img/cards/red_back.jpg" alt="front of card"/>
+      </div>
+      <div className="back">
+        <img src={imageSrc} alt="back of card"/>
+      </div>
+    </div>
+  );
+}
+
