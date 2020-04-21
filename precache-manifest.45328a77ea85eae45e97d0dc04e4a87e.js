ac8b7115d7a519e5835f50f8225cdd5ecf5ade25
Daniel Davis <ooblioob@gmail.com>
2020-04-21 16:49:15 -0400

    Updates

---
+++ precache-manifest.45328a77ea85eae45e97d0dc04e4a87e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec08c8a9653d8c8fb1fa49feea0af7a9",
+    "url": "/react-card-game/index.html"
+  },
+  {
+    "revision": "ff6ded15713ce93ea926",
+    "url": "/react-card-game/static/css/3.37b88233.chunk.css"
+  },
+  {
+    "revision": "e35652ad73d9ee72a0c9",
+    "url": "/react-card-game/static/css/main.21e6a027.chunk.css"
+  },
+  {
+    "revision": "fc847d832aa364f4475e",
+    "url": "/react-card-game/static/js/2.9947cf25.chunk.js"
+  },
+  {
+    "revision": "601708928e81de6a0992654bfa3f2122",
+    "url": "/react-card-game/static/js/2.9947cf25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff6ded15713ce93ea926",
+    "url": "/react-card-game/static/js/3.8686ebd3.chunk.js"
+  },
+  {
+    "revision": "e35652ad73d9ee72a0c9",
+    "url": "/react-card-game/static/js/main.27ed1414.chunk.js"
+  },
+  {
+    "revision": "e5ec87eb44c4c41c1647",
+    "url": "/react-card-game/static/js/runtime-main.3b373985.js"
+  }
+]);
