6e3348bab6cadbee083d7a21a92b13d0d5001f61
Daniel Davis <ooblioob@gmail.com>
2020-04-22 09:00:47 -0400

    Optimizations for production environment
    - Moved all images to use WebPack's import/require, so now images should
    get included in the bundle instead of pulled in as an unoptimized static
    asset
    - Updated the Auth0 login/logout code so we can actually use it when
    deployed to gh-pages. There were some subtle problems when the app isn't
    at the root url...

--- src/components/card.js
+++ src/components/card.js
@@ -2,12 +2,11 @@ import React from "react";
 import "./card.css";
 
 export default function Card(props) {
-  const flipCard = e => {
+  const flipCard = (e) => {
     props.onClicked(props.index); // call the parent's click handler
   };
 
-  // TODO Come back and fix this prefixing properly! 
-  const imageSrc = "react-card-game/img/cards/" + props.value + ".jpg";
+  const imageSrc = require(`../assets/img/cards/${props.value}.jpg`);
 
   return (
     <div
@@ -15,7 +14,10 @@ export default function Card(props) {
       className={["card", props.flipped ? "isFlipped" : ""].join(" ")}
     >
       <div className="front">
-        <img src="/react-card-game/img/cards/red_back.jpg" alt="front of card" />
+        <img
+          src={require("../assets/img/cards/Red_back.jpg")}
+          alt="front of card"
+        />
       </div>
       <div className="back">
         <img src={imageSrc} alt="back of card" />
