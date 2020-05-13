f84af77a40eb50afa504df88834c6f6a8959783b
Daniel Davis <ooblioob@gmail.com>
2020-05-13 10:12:38 -0400

    More cleanup - moving files, fixing tests, cleaning up unused imports

--- src/components/MaterialSidebar.jsx
+++ src/components/MaterialSidebar.jsx
@@ -1,25 +1,16 @@
-import React, {useState} from "react";
+import React from "react";
 import { useAuth0 } from "../react-auth0-spa";
-import Profile from "./profile";
-import Login from "./auth/Login";
-import { NavLink } from "react-router-dom";
 
-
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-
-const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   list: {
@@ -49,7 +40,7 @@ const Sidebar = (props) => {
   const { loading } = useAuth0();
 
   const classes = useStyles();
-  const theme = useTheme();
+  // const theme = useTheme();
 
   if (loading) {
     return null;
@@ -93,54 +84,6 @@ const Sidebar = (props) => {
     </Drawer>
   )
 };
-    // <nav
-    //   className="w3-sidebar w3-white w3-animate-left"
-    //   style={{
-    //     zIndex: 3,
-    //     width: "300px",
-    //     display: props.visible ? "block" : "none",
-    //   }}
-    //   id="mySidebar"
-    // >
-    //   <br />
-    //   <Profile />
-    //   <hr />
-    //   <div className="w3-container">
-    //     <h5>Dashboard</h5>
-    //   </div>
-    //   <div className="w3-bar-block">
-    //     <NavLink
-    //       exact
-    //       to="/react-card-game"
-    //       activeClassName="w3-blue"
-    //       className="w3-bar-item w3-button w3-padding"
-    //     >
-    //       <i className="fa fa-home fa-fw"></i> Home
-    //     </NavLink>
-    //     <NavLink
-    //       to="/react-card-game/instructions"
-    //       activeClassName="w3-blue"
-    //       className="w3-bar-item w3-button w3-padding"
-    //     >
-    //       <i className="fa fa-book fa-fw"></i>  Instructions
-    //     </NavLink>
-    //     <NavLink
-    //       to="/react-card-game/achievements"
-    //       activeClassName="w3-blue"
-    //       className="w3-bar-item w3-button w3-padding"
-    //     >
-    //       <i className="fa fa-trophy fa-fw"></i>  Achievements
-    //     </NavLink>
-    //     <NavLink
-    //       to="/react-card-game/settings"
-    //       activeClassName="w3-blue"
-    //       className="w3-bar-item w3-button w3-padding"
-    //     >
-    //       <i className="fa fa-cog fa-fw"></i>  Settings
-    //     </NavLink>
-    //     <Login />
-    //   </div>
-    // </nav>
 
 Sidebar.defaultProps = {
   visible: false,
